b4dd737a3a4efe8023a18ca6e223cf3a69b356fe
Quan Dai Tri <qdaitridn2003@gmail.com>
2023-09-17 00:50:45 +0700

    First initial app

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    printWidth: 100,
+    endOfLine: 'auto',
+  };
---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import Root from './root';
+
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Root />
+        </SafeAreaView>
+    );
+};
+
+export default App;
