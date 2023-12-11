a40d2ec60fc34287ca039b1e505f8d1d32e6fb24
duongvo2801 <vvduong2801@gmail.com>
2023-12-11 13:07:44 +0700

    add statistics

--- src/navigations/BottomNavigation.jsx
+++ src/navigations/BottomNavigation.jsx
@@ -1,5 +1,6 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import React from 'react';
 
 import Icon from '../components/common/Icon';
--- src/screens/employees/SearchBar.jsx
+++ src/screens/employees/SearchBar.jsx
@@ -1,86 +1,47 @@
-import { Feather, Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 import React from 'react';
-import { StyleSheet, TextInput, View, Keyboard, Button } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
-const SearchBar = ({ clicked, searchPhrase, setSearchPhrase, setClicked }) => {
+import { Color } from '../../components/styles/GlobalStyles';
+
+const SearchBar = ({ searchPhrase, setSearchPhrase }) => {
   return (
     <View style={styles.container}>
-      <View style={clicked ? styles.searchBar__clicked : styles.searchBar__unclicked}>
-        <Feather name="search" size={20} color="black" style={{ marginLeft: 1 }} />
+      <View style={styles.searchBar}>
+        <Feather name="search" size={20} color="black" style={{ marginLeft: 10 }} />
         <TextInput
           style={styles.input}
-          placeholder="Tìm kiếm nhân viên"
+          placeholder="Tìm kiếm..."
           value={searchPhrase}
           onChangeText={setSearchPhrase}
-          onFocus={() => {
-            setClicked(true);
-          }}
         />
-        {clicked && (
-          <Entypo
-            name="cross"
-            size={20}
-            color="black"
-            style={{ padding: 1 }}
-            onPress={() => {
-              setSearchPhrase('');
-            }}
-          />
-        )}
       </View>
-      {clicked && (
-        <View>
-          <Button
-            style={styles.buttonText}
-            title="Cancel"
-            onPress={() => {
-              Keyboard.dismiss();
-              setClicked(false);
-            }}
-          />
-        </View>
-      )}
     </View>
   );
 };
+
 export default SearchBar;
 
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'flex-start',
     alignItems: 'center',
-    flexDirection: 'row',
     marginTop: 10,
     width: '100%',
     margin: 5,
   },
-  searchBar__unclicked: {
+  searchBar: {
     padding: 10,
     flexDirection: 'row',
     width: '95%',
-    backgroundColor: '#d9dbda',
+    backgroundColor: Color.colorWhitesmoke,
     borderRadius: 15,
     alignItems: 'center',
-  },
-  searchBar__clicked: {
-    padding: 10,
-    flexDirection: 'row',
-    width: '80%',
-    backgroundColor: '#d9dbda',
-    borderRadius: 15,
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
   },
   input: {
     fontSize: 16,
     marginLeft: 10,
     width: '90%',
-  },
-  buttonText: {
-    fontSize: 18,
-    color: '#fff',
-    fontWeight: 'bold',
-    alignSelf: 'center',
-    textTransform: 'uppercase',
+    backgroundColor: Color.colorWhitesmoke,
   },
 });
