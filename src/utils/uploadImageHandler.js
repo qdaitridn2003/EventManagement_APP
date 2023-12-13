91d20ac236511f455bc0c6be8f4a13e25e1118de
Quan Dai Tri <qdaitridn2003@gmail.com>
2023-12-13 22:20:44 +0700

    Explain parameters of function uploadImage

--- src/utils/uploadImageHandler.js
+++ src/utils/uploadImageHandler.js
@@ -1,4 +1,12 @@
 /* eslint-disable import/namespace */
+/**
+ * Explain parameters of function
+ * path: Là đường dẫn đến route để upload image
+ * uri: Dùng thư viện expo-image-picker để lấy ra uri
+ * fieldName: Tức là field trên swagger nó nhận ví dụ như avatar,image,....
+ * accessToken: Chắc là mọi người tự hiểu cái này làm gì
+ */
+
 import * as FileSystem from 'expo-file-system';
 
 export const uploadImage = async (path, uri, fieldName, accessToken) => {
